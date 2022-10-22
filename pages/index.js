ef24c05101a730a14c8af228cfa9a7bce45ea667
Agas <andreyagassi.aa@gmail.com>
2022-10-23 02:35:47 +0700

    fourth commit

--- pages/index.js
+++ pages/index.js
@@ -11,21 +11,21 @@ export default function Home() {
     'This is My Portfolio.',
     'I Hope You Like It!'],
     loop: true,
-    delaySpeed: 3000,
+    delaySpeed: 1500,
   })
 
   return (
     <div>
       <Head>
         <title>Andrey Agassi Portofolio</title>
-        <meta name="description" content="Junior Website Developer" />
+        <meta name="description" content="Junior Web Developer" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className=' bg-white px-10 font-poppins'>
         <section className=' min-h-screen'>
           <NavBar/>
-          <h1 className=' text-5xl text-center p-20 py-60'>
+          <h1 className=' text-3xl md:text-4xl lg:text-5xl text-center px-20 py-60 max-h-80'>
             <span>{text}</span>
             < Cursor />
           </h1>
