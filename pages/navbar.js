c725192a797fe53b4662fa761aa44580074bd539
Agas <andreyagassi.aa@gmail.com>
2022-11-11 21:02:30 +0700

    PlaceHolder3

--- pages/navbar.js
+++ pages/navbar.js
@@ -4,7 +4,7 @@ import Logo from '../public/logo.png'
 export default function NavBar() {
   return (
           <nav className=' bg-white px-10 py-10 mb-12 flex justify-between md:px-10 lg:px-20'>
-            <Image src={Logo} alt="Logo" width="50px" height="50px" />
+            <Image src={Logo} width="50px" height="50px" />
             <ul className=' flex items-center  md:px-10 lg:px-10'>
               <li><a className=' pl-5 md:px-10 lg:px-10' href='https://www.linkedin.com/in/andrey-agassi'>LinkedIn</a></li>
               <li><a className=' pl-5 md:pl-10 lg:pl-10' href='#'>Resume</a></li>
